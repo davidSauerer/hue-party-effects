8a98d6a1f15665401f5b817f88145443fe23fa35
Superbum <superbum@MacBook-Pro-von-Superbum.local>
2022-10-07 17:11:22 +0200

    Make all sockets and bulbs configurable over args

--- README.md
+++ README.md
@@ -3,7 +3,16 @@ This project is controlling your hue bridge and enables some party effects
 
 ## How to get started
 Start the application with the ip address as Command-Line Argument. Then you need to press the button of the Hue bridge.
+ + --ip (IP of Hue Bridge)
+ + --bulbs (list of bulbs for the most effects)
+   + provides included bulbs for the most effects
+ + --bulbs-cycle (List of bulbs for Light cycle effect)
+   + provides included bulbs
+   + provides order for Light cycle effect
+ + --blacklight (Sockets for blacklight)
+   
 
+Example: --ip 192.168.1.100 --bulbs 15 1 24 26 42 53 2 52 --bulbs-cycle 1 26 17 24 53 2 52 --blacklight 50 48 
 ## Limitations
  + The used bulbs are hardcoded
  + You could not turn specific effects on or off
--- main.py
+++ main.py
@@ -86,12 +86,11 @@ def just_blacklight(percentage):
     return False
 
 
-def flash_light_effect(percentage):
+def flash_light_effect(percentage, circle):
     if random.randint(0, 100) > percentage:
         turn_off_all()
-        circle = [1, 26, 17, 24, 53, 2, 52]
         for _ in range(random.randint(2, 5)):
-            for lamp in lamps:
+            for lamp in circle:
                 command = {'on': True, 'bri': 254, 'sat': 254, 'hue': 10000}
                 b.set_light(lamp, command)
                 time.sleep(0.3)
@@ -103,25 +102,33 @@ def flash_light_effect(percentage):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Personal information')
     parser.add_argument('--ip', dest='ip', type=str, help='IP of the hue bridge')
+    parser.add_argument('--bulbs', dest='list_bulbs', nargs="+", type=int, help='List of bulbs the application should manage')
+    parser.add_argument('--bulbs-cycle', dest='list_bulbs_cycle', nargs="+", type=int,
+                        help='List of bulbs the application should call for the cycle blink mode')
+    parser.add_argument('--blacklight', dest='list_blacklight', nargs="+", type=int,
+                        help='List of bulbs the application should call for the cycle blink mode')
+
     args = parser.parse_args()
     b = Bridge(args.ip)
     # If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
     b.connect()
     # Get the bridge state (This returns the full dictionary that you can explore) with b.get_api()
 
-    lamps = [15, 1, 24, 26, 42, 53, 2, 52]
-    blackLight = [50, 48]
+    lamps = args.list_bulbs
+    cycle_lamps = args.list_bulbs_cycle
+    blackLight = args.list_blacklight
     lampsHue = []
     lampsBri = []
     lampsSat = []
 
-    for l in lamps:
-        lampsHue.append(b.get_light(l, 'hue'))
-        lampsSat.append(b.get_light(l, 'sat'))
-        lampsBri.append(b.get_light(l, 'bri'))
+    #save the current color configuration of the bulbs
+    for bulb in lamps:
+        lampsHue.append(b.get_light(bulb, 'hue'))
+        lampsSat.append(b.get_light(bulb, 'sat'))
+        lampsBri.append(b.get_light(bulb, 'bri'))
 
     while True:
-        flash_light_effect(99)
+        flash_light_effect(99, cycle_lamps)
 
         if just_blacklight(99):
             time.sleep(random.randint(30, 120))
@@ -137,4 +144,4 @@ if __name__ == '__main__':
                 time.sleep(random.randint(10, 30))
                 black_light_turn_off()
             else:
-                flash_light_effect(500)
+                flash_light_effect(500, cycle_lamps)
